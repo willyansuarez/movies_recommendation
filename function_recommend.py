b1bbb13d9deedc67e29bbff6aa56959912f707be
willyansuarez <122314286+willyansuarez@users.noreply.github.com>
2023-08-09 19:29:50 -0400

    Update function_recommend.py

--- function_recommend.py
+++ function_recommend.py
@@ -1,24 +1,37 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem.porter import PorterStemmer
-from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
+# from sklearn.feature_extraction.text import CountVectorizer
+# from nltk.stem.porter import PorterStemmer
+# from sklearn.metrics.pairwise import cosine_similarity
+# import pandas as pd
 
-new_df = pd.read_parquet("./datasets/movies_model_final.parquet")
+# new_df = pd.read_parquet("/home/willian/modelo_recomendacion_peliculas/movies_recommendation/datasets/movies_model_final.parquet")
 
-cv = CountVectorizer(max_features=500, stop_words="english")
-vectors = cv.fit_transform(new_df["tags"]).toarray()
+# cv = CountVectorizer(max_features=5000, stop_words="english")
+# vectors = cv.fit_transform(new_df["tags"]).toarray()
 
-similarity = cosine_similarity(vectors)
+# similarity = cosine_similarity(vectors)
 
-# def recommend(movie):
-#     movie_index = new_df[new_df["title"] == movie].index[0]
-#     distances = similarity[movie_index]
-#     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+# # def recommend(movie):
+# #     movie_index = new_df[new_df["title"] == movie].index[0]
+# #     distances = similarity[movie_index]
+# #     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     
-#     for i in movie_list:
-#         print(new_df.iloc[i[0]].title)
-#         # print(i[0])
-#     # return
+# #     for i in movie_list:
+# #         print(new_df.iloc[i[0]].title)
+# #         # print(i[0])
+# #     # return
+
+# # def recommend(movie):
+# #     movie_index = new_df[new_df["title"] == movie].index[0]
+# #     distances = similarity[movie_index]
+# #     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
+# #     pel = []
+# #     for i in movie_list:
+# #         list_pel = new_df.iloc[i[0]].title
+# #         pel.append(list_pel)
+# #         # print(i[0])
+# #     return pel
+
 
 # def recommend(movie):
 #     movie_index = new_df[new_df["title"] == movie].index[0]
@@ -32,20 +45,7 @@ similarity = cosine_similarity(vectors)
 #         # print(i[0])
 #     return pel
 
-
-def recommend(movie):
-    movie_index = new_df[new_df["title"] == movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    
-    pel = []
-    for i in movie_list:
-        list_pel = new_df.iloc[i[0]].title
-        pel.append(list_pel)
-        # print(i[0])
-    return pel
-
-# recommend("Lethal Weapon")
+# # recommend("Lethal Weapon")
 
 
 
