8c3bf4b80615c40e891b66dd462e21536f920338
Willian <willyanjosesuarez@gmail.com>
2024-03-26 12:07:50 -0400

    prueba ruta datos 8

--- main.py
+++ main.py
@@ -8,16 +8,10 @@ from fastapi import HTTPException
 
 # df = pd.read_csv("/home/willian/modelo_recomendacion_peliculas/movies_recommendation/datasets/movies_processed.csv")
 # df_2 = pd.read_parquet("/home/willian/modelo_recomendacion_peliculas/movies_recommendation/datasets/movies_with_recommendations.parquet")
-df = pd.read_csv("movies_processed.csv")
 
-# df = pd.read_csv("./datasets/movies_processed.csv")
-# df = pd.read_csv("datasets/movies_processed.csv") # 1
-# df = pd.read_csv("movies_recommendation/datasets/movies_processed.csv") # 2
-# df = pd.read_csv("./datasets/movies_processed.csv") # 3
-# df = pd.read_csv("/datasets/movies_processed.csv") # 4
-# df = pd.read_csv("/movies_processed.csv") # 5
-# df = pd.read_csv("./movies_processed.csv") # 6
-# df_2 = pd.read_parquet("./datasets/movies_with_recommendations.parquet")
+
+df = pd.read_csv("datasets/movies_processed.csv")
+df_2 = pd.read_parquet("datasets/movies_with_recommendations.parquet")
 
 app = FastAPI(
     title = "Películas: Consultas y Recomendaciones", 
@@ -48,7 +42,6 @@ app.add_middleware(
 async def peliculas_idioma( Idioma: str ):
     ''' Se ingresa un idioma (como están escritos en el dataset, no hay que traducirlos!). 
     Debe devolver la cantidad de películas producidas en ese idioma. '''        
-    # df = pd.read_csv("movies_processed.csv")
     res = df["original_language"] == Idioma
     return {"idioma": Idioma, "cantidad": str(res.sum())}
 
@@ -56,7 +49,6 @@ async def peliculas_idioma( Idioma: str ):
 # @app.get('/peliculas_duracion/{pelicula}')
 # async def peliculas_duracion( Pelicula: str ):
 #     ''' Se ingresa una pelicula. Debe devolver la duracion y el año. '''
-#     df = pd.read_csv("movies_processed.csv")
 #     res = df[df["title"] == Pelicula]
 #     return {"pelicula": Pelicula, "duracion": res.runtime.item(), "anio": res.release_year.item()}
 
@@ -65,7 +57,6 @@ async def peliculas_idioma( Idioma: str ):
 # async def franquicia( Franquicia: str ):
 #     '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia
 #     total y promedio '''
-#     df = pd.read_csv("movies_processed.csv")
 #     f = df[df["belongs_to_collection"] == Franquicia]
 #     cant_pel = len(f)
 #     ganancia_total = f.revenue.sum()
@@ -92,7 +83,6 @@ async def peliculas_idioma( Idioma: str ):
 # async def productoras_exitosas( Productora: str ):
 #     '''Se ingresa la productora, entregandote el revenue total y la cantidad
 #     de peliculas que realizó.'''
-#     df = pd.read_csv("movies_processed.csv")
 #     producer = df[df.production_companies.str.contains(Productora)]
 #     return {
 #         'productora':Productora,
@@ -104,9 +94,7 @@ async def peliculas_idioma( Idioma: str ):
 # @app.get('/get_director/{nombre_director}')
 # async def get_director( nombre_director: str ):
 #     ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma, en formato lista.'''
-#     df = pd.read_csv("movies_processed.csv")
 #     director = df.loc[df['crew'].str.contains(nombre_director)]
-
 #     revenue_total = sum(director.revenue) / sum(director.budget)
 #     success_director = {        
 #         'medicion_exito': revenue_total        
@@ -142,7 +130,6 @@ async def peliculas_idioma( Idioma: str ):
 # @app.get('/recomendacion/{titulo}')
 # async def recomendacion(titulo:str):
 #     '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
-#     df_2 = pd.read_parquet("movies_with_recommendations.parquet")
 #     res = tuple(df_2[df_2["movie"] == titulo]["recommended"].item())
     
 #     return {res}    
