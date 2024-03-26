34e8b7754f169507b58844b4f91d7c21f896b962
Willian <willyanjosesuarez@gmail.com>
2024-03-26 09:40:22 -0400

    prueba ruta datos 6

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ import os
 # df = pd.read_csv("movies_recommendation/datasets/movies_processed.csv") # 2
 # df = pd.read_csv("./datasets/movies_processed.csv") # 3
 # df = pd.read_csv("/datasets/movies_processed.csv") # 4
-df = pd.read_csv("/movies_processed.csv") # 5
+# df = pd.read_csv("/movies_processed.csv") # 5
+df = pd.read_csv("./movies_processed.csv") # 6
 # df_2 = pd.read_parquet("./datasets/movies_with_recommendations.parquet")
 
 app = FastAPI()
