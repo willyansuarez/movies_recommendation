371a906a4dfc6d4cf4548e25e5b5c63ea1136926
Willian <willyanjosesuarez@gmail.com>
2024-03-26 16:35:46 -0400

    arregladas las rutas, titulo y agregado cors

--- main.py
+++ main.py
@@ -46,90 +46,90 @@ async def peliculas_idioma( Idioma: str ):
     return {"idioma": Idioma, "cantidad": str(res.sum())}
 
 
-# @app.get('/peliculas_duracion/{pelicula}')
-# async def peliculas_duracion( Pelicula: str ):
-#     ''' Se ingresa una pelicula. Debe devolver la duracion y el año. '''
-#     res = df[df["title"] == Pelicula]
-#     return {"pelicula": Pelicula, "duracion": res.runtime.item(), "anio": res.release_year.item()}
+@app.get('/peliculas_duracion/{pelicula}')
+async def peliculas_duracion( Pelicula: str ):
+    ''' Se ingresa una pelicula. Debe devolver la duracion y el año. '''
+    res = df[df["title"] == Pelicula]
+    return {"pelicula": Pelicula, "duracion": res.runtime.item(), "anio": res.release_year.item()}
 
 
-# @app.get('/franquicia/{franquicia}')
-# async def franquicia( Franquicia: str ):
-#     '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia
-#     total y promedio '''
-#     f = df[df["belongs_to_collection"] == Franquicia]
-#     cant_pel = len(f)
-#     ganancia_total = f.revenue.sum()
-#     promed = ganancia_total / 3
-#     return {'franquicia':Franquicia, 'cantidad':cant_pel, 'ganancia_total':ganancia_total, 'ganancia_promedio':round(promed, 2)}
+@app.get('/franquicia/{franquicia}')
+async def franquicia( Franquicia: str ):
+    '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia
+    total y promedio '''
+    f = df[df["belongs_to_collection"] == Franquicia]
+    cant_pel = len(f)
+    ganancia_total = f.revenue.sum()
+    promed = ganancia_total / 3
+    return {'franquicia':Franquicia, 'cantidad':cant_pel, 'ganancia_total':ganancia_total, 'ganancia_promedio':round(promed, 2)}
 
 
-# @app.get('/peliculas_pais/{pais}')
-# async def peliculas_pais( Pais: str ):
-#     df = pd.read_csv("movies_processed.csv")
-#     cantidad = df['production_countries'].str.contains(Pais, case=False).sum()
-#     datos = dict()
+@app.get('/peliculas_pais/{pais}')
+async def peliculas_pais( Pais: str ):
+    df = pd.read_csv("movies_processed.csv")
+    cantidad = df['production_countries'].str.contains(Pais, case=False).sum()
+    datos = dict()
     
-#     if cantidad > 0:
-#         datos = {
-#                   "pais"    : Pais,
-#                   "cantidad": str(cantidad)
-#         }
+    if cantidad > 0:
+        datos = {
+                  "pais"    : Pais,
+                  "cantidad": str(cantidad)
+        }
         
-#     return datos
+    return datos
 
 
-# @app.get('/productoras_exitosas/{productora}')
-# async def productoras_exitosas( Productora: str ):
-#     '''Se ingresa la productora, entregandote el revenue total y la cantidad
-#     de peliculas que realizó.'''
-#     producer = df[df.production_companies.str.contains(Productora)]
-#     return {
-#         'productora':Productora,
-#         'revenue_total': str(producer.revenue.sum()),
-#         'cantidad':str(producer.title.count())
-#     }
+@app.get('/productoras_exitosas/{productora}')
+async def productoras_exitosas( Productora: str ):
+    '''Se ingresa la productora, entregandote el revenue total y la cantidad
+    de peliculas que realizó.'''
+    producer = df[df.production_companies.str.contains(Productora)]
+    return {
+        'productora':Productora,
+        'revenue_total': str(producer.revenue.sum()),
+        'cantidad':str(producer.title.count())
+    }
 
 
-# @app.get('/get_director/{nombre_director}')
-# async def get_director( nombre_director: str ):
-#     ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma, en formato lista.'''
-#     director = df.loc[df['crew'].str.contains(nombre_director)]
-#     revenue_total = sum(director.revenue) / sum(director.budget)
-#     success_director = {        
-#         'medicion_exito': revenue_total        
-#     }
+@app.get('/get_director/{nombre_director}')
+async def get_director( nombre_director: str ):
+    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma, en formato lista.'''
+    director = df.loc[df['crew'].str.contains(nombre_director)]
+    revenue_total = sum(director.revenue) / sum(director.budget)
+    success_director = {        
+        'medicion_exito': revenue_total        
+    }
     
-#     movies = list(director.title)
-#     rel_years = list(director.release_year)
-#     ind_returns = list(round(director["return"], 2))
-#     costs = list(director.budget)
-#     revs = list(director.revenue)
+    movies = list(director.title)
+    rel_years = list(director.release_year)
+    ind_returns = list(round(director["return"], 2))
+    costs = list(director.budget)
+    revs = list(director.revenue)
     
-#     movie_data = []
-#     for movie, rel_year, ind_return, cost, rev in zip(movies, rel_years, ind_returns, costs, revs):
-#         movie_data.append([movie, rel_year, ind_return, cost, rev])
+    movie_data = []
+    for movie, rel_year, ind_return, cost, rev in zip(movies, rel_years, ind_returns, costs, revs):
+        movie_data.append([movie, rel_year, ind_return, cost, rev])
     
-#     movies_dict = {}
+    movies_dict = {}
     
-#     for movie, rel_year, ind_return, cost, rev in movie_data:
-#         movies_dict[movie] = {
-#             "año": rel_year,
-#             "retorno": ind_return,
-#             "costo": cost,
-#             "ganancia": rev
-#     }
+    for movie, rel_year, ind_return, cost, rev in movie_data:
+        movies_dict[movie] = {
+            "año": rel_year,
+            "retorno": ind_return,
+            "costo": cost,
+            "ganancia": rev
+    }
 
-#     return movies_dict, success_director
+    return movies_dict, success_director
     
     
     
 
 
-# # # ML
-# @app.get('/recomendacion/{titulo}')
-# async def recomendacion(titulo:str):
-#     '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
-#     res = tuple(df_2[df_2["movie"] == titulo]["recommended"].item())
+# # ML
+@app.get('/recomendacion/{titulo}')
+async def recomendacion(titulo:str):
+    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
+    res = tuple(df_2[df_2["movie"] == titulo]["recommended"].item())
     
-#     return {res}    
+    return {res}    
